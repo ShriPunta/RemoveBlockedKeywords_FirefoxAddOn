6a222fb6df7efa66669198fa3a10560cde44dcd1
Shridhar Puntambekar <shridhar.puntambekar@gmail.com>
2025-08-13 16:43:38 -0400

    migration to popup.ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: {
+    index: './src/index.ts',
+    popup: './src/popup/popup.ts',
+  },
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    filename: '[name].js',
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.txt$/,
